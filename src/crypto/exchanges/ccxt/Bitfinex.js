01d406af7c2cf3038ea44487063e48850a2b1946
Luis Fernando Pardo <lfern70@gmail.com>
2023-05-21 20:43:01 +0200

    filter paper/no-paper markets

--- src/crypto/exchanges/ccxt/Bitfinex.js
+++ src/crypto/exchanges/ccxt/Bitfinex.js
@@ -11,6 +11,17 @@ class Bitfinex extends BaseExchangeCcxt {
         super('bitfinex2', params);
     }
 
+    /** @inheritdoc */
+    async loadMarkets(reload = false) {
+        let markets = await super.loadMarkets(reload);
+
+        if (this.params.paper) {
+            return Object.fromEntries(Object.entries(markets).filter(([k,v]) => k.startsWith('TEST')));
+        } else {
+            return Object.fromEntries(Object.entries(markets).filter(([k,v]) => !k.startsWith('TEST')));
+        }
+    }
+
 }
 
 module.exports = { Bitfinex }
