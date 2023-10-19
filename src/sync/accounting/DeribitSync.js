991fc67b67c043d3299af3d509c53fa95de56197
Luis Fernando Pardo <lfern70@gmail.com>
2023-10-19 11:28:07 +0200

    add executions from ledger

--- src/sync/accounting/DeribitSync.js
+++ src/sync/accounting/DeribitSync.js
@@ -1,13 +1,15 @@
 const { BaseExchangeSync } = require("./BaseExchangeSync");
 const models = require("../../../models");
-const { BaseExchangeCcxtTrade } = require("../../crypto/exchanges/ccxt/BaseExchangeCcxtTrade");
 const { LedgerTypes } = require("../../crypto/exchanges/BaseExchange");
 const { default: BigNumber } = require("bignumber.js");
 const { create } = require("lodash");
 const { Deribit } = require("../../crypto/exchanges/Deribit");
+const { BaseExchangeCcxtTrade } = require("../../crypto/exchanges/ccxt/BaseExchangeCcxtTrade");
+const { safeString, safeFloat } = require("../../crypto/exchanges/ccxt/safeUtils");
 
 /** @typedef {import('../../crypto/exchanges/BaseExchange').ExtendedLedgerEntry} ExtendedLedgerEntry */
 /** @typedef {import("../../crypto/exchanges/BaseExchange").UserInfo} UserInfo */
+/** @typedef {import("../../crypto/exchanges/ccxt/BaseExchangeCcxtTrade").BaseExchangeCcxtTrade} BaseExchangeCcxtTrade */
 
 class DeribitSync extends BaseExchangeSync {
     async sync(accountId) {
@@ -19,8 +21,9 @@ class DeribitSync extends BaseExchangeSync {
         }
 
         let created = await this.syncLedger(accountId, userDict);
-        let created2 = await this.syncTrades(accountId, userDict);
-        return created | created2;
+        return created;
+        //let created2 = await this.syncTrades(accountId, userDict);
+        //return created | created2;
     }
 
     async syncLedger(accountId, userDict) {
@@ -54,8 +57,16 @@ class DeribitSync extends BaseExchangeSync {
                     }
                 }
 
+
                 curCreated = await this.repository.addLedger(accountId, this.exchange.getExchangeName(), holder, entry);
                 created = created | curCreated;
+
+                if (entry.type == 'trade') {
+                    let trade = this.generateTradeFromExtenderLedgerEntry(entry);
+                    curCreated = await this.repository.addExecution(accountId, this.exchange.getExchangeName(), holder, trade);
+                    created = created | curCreated;
+                }
+
                 lastTs = entry.timestamp;
             }
 
@@ -68,35 +79,35 @@ class DeribitSync extends BaseExchangeSync {
 
         return globalCreate;
     }
-
-    async syncTrades(accountId, userDict) {
-        let lastTs = null;
-        let endpoint = await this.repository.getLastSyncEndpoint(accountId, 'trades');
-        let since = 1000;
-        if (endpoint != null){
-            since = endpoint.last_ts;
-        }
-
-        let limit = 1000;
-        let entries = await this.exchange.fetchMyTrades(undefined, since, limit, {sort: 1});
-        let created = false;
-        console.log(`DeribitSync: ${accountId} last trades request response lenght: ${entries.length}`)
-        for(let i=0;i<entries.length;i++) {
-            let entry = entries[i];
-            /** @type {UserInfo} */
-            let userInfo = this.exchange.ccxtExchange.safeValue(userDict, entry.account);
-            let holder = this.exchange.ccxtExchange.safeString(userInfo, 'holder');            
-            let curCreated = await this.repository.addExecution(accountId, this.exchange.getExchangeName(), holder, entry);
-            created = created | curCreated;
-            lastTs = entry.timestamp;
-        }
-
-        if (lastTs != null) {
-            await this.repository.setLastSyncEndpoint(accountId, 'trades', lastTs);
-        }
-
-        return created;
-    }
+    // Use ledger to sync trades
+    //async syncTrades(accountId, userDict) {
+    //    let lastTs = null;
+    //    let endpoint = await this.repository.getLastSyncEndpoint(accountId, 'trades');
+    //    let since = 1000;
+    //    if (endpoint != null){
+    //        since = endpoint.last_ts;
+    //    }
+    //
+    //    let limit = 1000;
+    //    let entries = await this.exchange.fetchMyTrades(undefined, since, limit, {sort: 1});
+    //    let created = false;
+    //    console.log(`DeribitSync: ${accountId} last trades request response lenght: ${entries.length}`)
+    //    for(let i=0;i<entries.length;i++) {
+    //        let entry = entries[i];console.log("trade------");console.log(entry);
+    //        /** @type {UserInfo} */
+    //        let userInfo = this.exchange.ccxtExchange.safeValue(userDict, entry.account);
+    //        let holder = this.exchange.ccxtExchange.safeString(userInfo, 'holder');            
+    //        let curCreated = await this.repository.addExecution(accountId, this.exchange.getExchangeName(), holder, entry);
+    //        created = created | curCreated;
+    //        lastTs = entry.timestamp;
+    //    }
+    //
+    //    if (lastTs != null) {
+    //        await this.repository.setLastSyncEndpoint(accountId, 'trades', lastTs);
+    //    }
+    //
+    //    return created;
+    //}
 
     /**
      * 
@@ -143,6 +154,44 @@ class DeribitSync extends BaseExchangeSync {
         entry.id = "" + entry.id + "@1";
         entry.before = newEntry.after;
         return newEntry;
+    }
+
+    /**
+     * 
+     * @param {ExtendedLedgerEntry} entry 
+     * @returns {BaseExchangeCcxtTrade}
+     */
+    generateTradeFromExtenderLedgerEntry(entry) {
+        let symbol = safeString(entry.info, 'instrument_name');
+        if (symbol in this.exchange.marketsById) {
+            const markets = this.exchange.marketsById[symbol];
+            if (markets.length > 0) { 
+                symbol = markets[0]['symbol'];
+            }
+        }
+
+        let cost = safeFloat(entry.info, 'cashflow');
+        if (cost < 0) {
+            cost = -cost;
+        }
+
+        // we only fill required fields for our database
+        return new BaseExchangeCcxtTrade({
+            account: entry.account,
+            wallet: entry.newFWallet,
+            symbol: symbol,
+            order: safeString(entry.info,'order_id'),
+            id: safeString(entry.info, 'trade_id'),
+            timestamp: entry.timestamp,
+            datetime: entry.datetime,
+            price: safeFloat(entry.info, 'price'),
+            amount: safeFloat(entry.info, 'amount'),
+            cost,
+            fee: {
+                cost: entry.fee != undefined ? entry.fee.cost : 0,
+                currency: entry.fee != undefined ? entry.fee.currency : '',
+            }
+        });
     }
 
     
